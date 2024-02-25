51b6c5afd5f980129f72877285c393d97429ed85
ismox3 <ismox3@gmail.com>
2024-02-25 18:48:25 -0400

    Trigger to validates password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
     has_secure_password
 
+    before_update :validates_change_password
+
     validates :email, presence: true, uniqueness: true
     validates :dni, presence: true, uniqueness: true
     validates :name, presence: true
     validates :password,
-              length: { minimum:8 }
+              length: { minimum:8 },
+              if: -> { password.length > 0 }
 
     def generate_password_token
         self.reset_password_token = SecureRandom.hex(3).upcase!
@@ -15,5 +18,12 @@ class User < ApplicationRecord
 
     def token_is_valid?(token = '')
         token == self.reset_password_token
+    end
+
+    def validates_change_password
+        @password = self.password
+
+        self.password = @password
+        save
     end
 end
